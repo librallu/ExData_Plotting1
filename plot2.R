609189536a2e534a160871511af08c2db1cccd3c
Luc LIBRALESSO <libralesso.l@gmail.com>
2014-08-20 12:32:45 +0200

    add some comments in plot2.R

--- plot2.R
+++ plot2.R
@@ -18,9 +18,17 @@ png("plot2.png", width=480, height=480)
 
 # trace plot
 x <- c(1:nrow(data))
+
+# initial plot but remove axes to allow custom axis
 plot(x, data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)", axes=F)
+
+# x axis with 3 positions : 0, middle of x and last value of x
 axis(side=1, at=c(0,nrow(data)/2,nrow(data)), labels=c("Thu","Fri","Sat"))
+
+# simply use y axis
 axis(side=2)
+
+# draw a box around the plot
 box(which="plot", lty="solid")
 
 # save to plot2.png
